d287d51504eaf0943f8ede6269156c7af3b829ec
sanskritimishra10 <sanskritimishra30599@gmail.com>
2023-07-22 22:37:17 +0530

    Task1_CSI

---
+++ Task1/src/pages/Desktop1.js
@@ -0,0 +1,283 @@
+import "./Desktop1.css";
+const Desktop1 = () => {
+  return (
+    <div className="desktop-1">
+      <img className="desktop-1-child" alt="" src="/rectangle-1@2x.png" />
+      <nav className="zm-ve-hizmetler-parent">
+        <b className="zm-ve-hizmetler">Çözüm ve Hizmetler</b>
+        <b className="zm-ve-hizmetler">Ürünler</b>
+        <b className="zm-ve-hizmetler">Teknolojiler</b>
+        <b className="zm-ve-hizmetler">İnsan Kaynakları</b>
+        <b className="zm-ve-hizmetler">Kurumsal</b>
+      </nav>
+      <div className="letiim-wrapper">
+        <b className="zm-ve-hizmetler">İletişim</b>
+      </div>
+      <div className="bilgi-teknolojilerinde-23-yll-parent">
+        <div className="bilgi-teknolojilerinde-23-container">
+          <p className="bilgi-teknolojilerinde">Bilgi Teknolojilerinde</p>
+          <p className="bilgi-teknolojilerinde">23 Yıllık Tecrübe</p>
+        </div>
+        <div className="mterilerimizin-yksek-kalite">
+          Müşterilerimizin yüksek kalite seviyelerini koruyabilmeleri için
+          farklı sektörlerde tecrübe kazanmış uzman kadrolarımızla Proje
+          Yönetimi, İş Analizi ve Test Yönetimi hizmetleri sunmaktayız.
+        </div>
+        <div className="mail-input">
+          <div className="mail-input-child" />
+          <div className="mail-bltenimize-kayt">Mail bültenimize kayıt ol</div>
+          <div className="mail-input-item" />
+          <b className="kayt-ol">Kayıt Ol</b>
+        </div>
+      </div>
+      <div className="stats-1">
+        <div className="title-description-8-col-parent">
+          <div className="title-description-8-col">
+            <div className="title-8-col-center-align">
+              <div className="section-title">
+                Test Yönetimiyle Neler Sağlıyoruz?
+              </div>
+            </div>
+            <div className="description">IoT Destekli Çözümler</div>
+          </div>
+          <div className="yazlm-kalitesini-arttryoru-parent">
+            <b className="yazlm-kalitesini-arttryoru-container">
+              <p className="bilgi-teknolojilerinde">Yazılım Kalitesini</p>
+              <p className="bilgi-teknolojilerinde">Arttırıyoruz</p>
+            </b>
+            <b className="olas-hatalar-nceden-container">
+              <p className="bilgi-teknolojilerinde">Olası Hataları Önceden</p>
+              <p className="bilgi-teknolojilerinde">Belirliyoruz</p>
+            </b>
+            <b className="oluabilecek-riskleri-nlyoru-container">
+              <p className="bilgi-teknolojilerinde">Oluşabilecek Riskleri</p>
+              <p className="bilgi-teknolojilerinde">Önlüyoruz</p>
+            </b>
+            <b className="zaman-ve-maliyetten-container">
+              <p className="bilgi-teknolojilerinde">Zaman ve Maliyetten</p>
+              <p className="bilgi-teknolojilerinde">Tasarruf Sağlıyoruz</p>
+            </b>
+            <img
+              className="caretcircledoubleright-icon"
+              alt=""
+              src="/caretcircledoubleright.svg"
+            />
+            <img
+              className="caretcircledoubleright-icon1"
+              alt=""
+              src="/caretcircledoubleright.svg"
+            />
+            <img
+              className="caretcircledoubleright-icon2"
+              alt=""
+              src="/caretcircledoubleright.svg"
+            />
+          </div>
+          <img className="image-1-icon" alt="" src="/image-1@2x.png" />
+        </div>
+      </div>
+      <div className="value-parent">
+        <div className="value">
+          <img
+            className="illustration-icon"
+            alt=""
+            src="/illustration@2x.png"
+          />
+          <div className="button-parent">
+            <div className="button">
+              <div className="kefet">Keşfet</div>
+            </div>
+            <div className="deneyimli-kadromuz-birok">
+              Deneyimli kadromuz; birçok farklı sektörden deneyimli, PMP®
+              sertifikasına sahip, uzman proje yöneticilerinden oluşmaktadır.
+              Projelerinizin yönetimine destek olmak, zayıf olduğunu
+              düşündüğünüz alanları güçlendirmek, uluslararası geçerliliği olan
+              PMI® metodolojisini şirketinizde uygulamak ve geliştirmek için
+              yanınızdayız.
+            </div>
+            <b className="kalite-ve-sre">Kalite ve Süreç Yönetimi</b>
+          </div>
+        </div>
+        <div className="value1">
+          <img
+            className="illustration-icon1"
+            alt=""
+            src="/illustration1@2x.png"
+          />
+          <div className="button-group">
+            <button className="button1" disabled>
+              <div className="kefet1">Keşfet</div>
+            </button>
+            <div className="danmanln-yrttmz-pr">
+              Danışmanlığını yürüttüğümüz projelerde yazılım hatalarını önlemek
+              amacıyla gerçekleştirdiğimiz testleri her proje aşamasında manuel
+              ya da test otomasyonu ile dikkatle uygulamaktayız. Deneyimli test
+              ekibimiz, kurumlara yüksek kalitede yazılım testleri hizmeti ile
+              baştan sona güvenli, kaliteli ve tatmin edici bir müşteri deneyimi
+              sunmaktadır.
+            </div>
+            <b className="test-ynetimi-ve">Test Yönetimi ve Analizi</b>
+          </div>
+        </div>
+      </div>
+      <footer className="footer">
+        <div className="background" />
+        <div className="copyright-2010-2021-">
+          © Copyright 2010-2021 - Can Çevik
+        </div>
+        <div className="section-5-parent">
+          <div className="section-5">
+            <div className="uzman-talep-formu">Uzman Talep Formu</div>
+            <div className="bilgi-stek-formu">Bilgi İstek Formu</div>
+            <b className="letiim1">İletişim</b>
+          </div>
+          <div className="section-4">
+            <div className="ortaklar">İş Ortakları</div>
+            <div className="uzman-talep-formu">Belge ve Yetkinlikler</div>
+            <div className="bilgi-stek-formu">Hakkımızda</div>
+            <b className="letiim1">Kurumsal</b>
+          </div>
+          <div className="section-3">
+            <div className="erik-ynetim-sistemi">İçerik Yönetim Sistemi</div>
+            <div className="ortaklar">Müşteri İlişkileri Yönetim Sistemi</div>
+            <div className="uzman-talep-formu">
+              İnsan Sermayesi Yönetim Sistemi
+            </div>
+            <div className="bilgi-stek-formu">Eğitim Yönetim Sistemi</div>
+            <b className="letiim1">Ürünler</b>
+          </div>
+          <div className="section-2">
+            <div className="iot-destekli-zmler">IoT Destekli Çözümler</div>
+            <div className="erik-ynetim-sistemi">Danışmanlık</div>
+            <div className="ortaklar">Kalite ve Süreç Yönetimi</div>
+            <div className="uzman-talep-formu">Outsourcing</div>
+            <div className="bilgi-stek-formu">Yazılım Geliştirme</div>
+            <b className="letiim1">Çözüm ve Hizmetler</b>
+          </div>
+        </div>
+      </footer>
+      <div className="bize-ulan-parent">
+        <div className="bize-ulan">Bize Ulaşın</div>
+        <div className="zm-hizmet-ve">
+          Çözüm, hizmet ve ürünlerimiz hakkında detaylı bilgi için bizimle
+          irtibata geçin.
+        </div>
+        <div className="frame-child" />
+        <img className="users-icon" alt="" src="/users.svg" />
+        <div className="bize-ulan-wrapper">
+          <div className="bize-ulan1">Bize Ulaşın</div>
+        </div>
+      </div>
+      <div className="logo-clouds-3">
+        <div className="section-heading">
+          <div className="title-8-col-center-align">
+            <div className="section-title1">Referanslarımız</div>
+          </div>
+          <div className="description1">
+            Bize güvenen ve beraber çalıştığımız iş ortaklarımız
+          </div>
+        </div>
+        <div className="logos">
+          <div className="col-1">
+            <div className="airbnb-logo">
+              <div className="bg" />
+              <img className="image-6-icon" alt="" src="/image-6@2x.png" />
+            </div>
+          </div>
+          <div className="col-2">
+            <div className="airbnb-logo">
+              <div className="bg" />
+              <img className="image-5-icon" alt="" src="/image-5@2x.png" />
+            </div>
+            <img
+              className="bookmyshow-logo-icon"
+              alt=""
+              src="/bookmyshow-logo.svg"
+            />
+          </div>
+          <div className="col-2">
+            <div className="airbnb-logo">
+              <div className="bg" />
+              <img className="image-2-icon" alt="" src="/image-2@2x.png" />
+            </div>
+            <div className="airbnb-logo">
+              <div className="bg" />
+              <img className="image-3-icon" alt="" src="/image-3@2x.png" />
+              <div className="fedex-logo1" />
+            </div>
+            <div className="airbnb-logo">
+              <div className="bg" />
+              <div className="tmn-gr-parent">
+                <div className="tmn-gr">Tümünü Gör</div>
+                <img className="arrowright-icon" alt="" src="/arrowright.svg" />
+              </div>
+            </div>
+          </div>
+          <div className="col-2">
+            <div className="airbnb-logo">
+              <div className="bg" />
+              <img className="image-4-icon" alt="" src="/image-4@2x.png" />
+            </div>
+            <img className="bookmyshow-logo-icon" alt="" src="/ola-logo.svg" />
+          </div>
+          <div className="col-1">
+            <img className="airbnb-logo" alt="" src="/amazon-logo.svg" />
+          </div>
+        </div>
+      </div>
+      <div className="mterilerimizin-yksek-kalite-parent">
+        <div className="mterilerimizin-yksek-kalite1">
+          Müşterilerimizin yüksek kalite seviyelerini koruyabilmeleri için
+          farklı sektörlerde tecrübe kazanmış uzman kadrolarımızla Proje
+          Yönetimi, İş Analizi ve Test Yönetimi hizmetleri sunmaktayız.
+        </div>
+        <div className="kalite-ve-sre2">Kalite ve Süreç Yönetimi</div>
+        <div className="dkman-analizi-parent">
+          <div className="dkman-analizi">Döküman Analizi</div>
+          <div className="ak-diyagramlar">Akış Diyagramları</div>
+          <div className="kurallar-analizi">İş Kuralları Analizi</div>
+          <div className="prototipleme">Prototipleme</div>
+          <div className="kabul-ve-deerlendirme">Kabul ve Değerlendirme</div>
+          <div className="payda-analizi">Paydaş Analizi</div>
+          <div className="ante-vulputate-ut">
+            Ante vulputate ut at accumsan et. Feugiat at tempus, est senectus
+            amet, elementum.
+          </div>
+          <div className="ante-vulputate-ut1">
+            Ante vulputate ut at accumsan et. Feugiat at tempus, est senectus
+            amet, elementum.
+          </div>
+          <div className="ante-vulputate-ut2">
+            Ante vulputate ut at accumsan et. Feugiat at tempus, est senectus
+            amet, elementum.
+          </div>
+          <div className="ante-vulputate-ut3">
+            Ante vulputate ut at accumsan et. Feugiat at tempus, est senectus
+            amet, elementum.
+          </div>
+          <div className="ante-vulputate-ut4">
+            Ante vulputate ut at accumsan et. Feugiat at tempus, est senectus
+            amet, elementum.
+          </div>
+          <div className="ante-vulputate-ut5">
+            Ante vulputate ut at accumsan et. Feugiat at tempus, est senectus
+            amet, elementum.
+          </div>
+          <div className="frame-item" />
+          <div className="frame-inner" />
+          <div className="ellipse-div" />
+          <div className="frame-child1" />
+          <img className="chartpieslice-icon" alt="" src="/chartpieslice.svg" />
+          <img className="figmalogo-icon" alt="" src="/figmalogo.svg" />
+          <img className="group-icon" alt="" src="/group-11.svg" />
+          <img className="star-icon" alt="" src="/star.svg" />
+          <img className="crown-icon" alt="" src="/crown.svg" />
+          <img className="frame-child2" alt="" src="/group-10.svg" />
+        </div>
+      </div>
+      <img className="desktop-1-item" alt="" src="/group-8.svg" />
+    </div>
+  );
+};
+
+export default Desktop1;
